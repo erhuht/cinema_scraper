61a23003a9f1ee4f7ca0ab2244cfb8b3c7f81790
erhuht <erhuht15@gmail.com>
2025-07-24 21:36:41 +0300

    Add subtitles to sections

--- newsletter/newsletter.py
+++ newsletter/newsletter.py
@@ -2,21 +2,23 @@ from bs4 import BeautifulSoup
 from pathlib import Path
 import re
 from datetime import datetime
+import copy
 
 
 def populate_html(movies):
     with open(Path(__file__).parent / 'template.html', encoding="utf-8") as template:
         soup = BeautifulSoup(template.read(), "html.parser")
 
+    subtitle_section = soup.find('div', attrs={'class': 'subtitle-section'})
+    movie_section = soup.find('div', attrs={'class': 'movie-section'})
     movie_template = soup.find('div', attrs={'class': 'movie'})
-    html_start = str(soup)[:str(soup).find(str(movie_template))]
+    html_start = str(soup)[:str(soup).find(str(subtitle_section))]
     html_end = str(soup)[str(soup).find(
-        str(movie_template))+len(str(movie_template)):]
+        str(movie_section))+len(str(movie_section)):]
 
     title = datetime.now().strftime("Helsingin elokuvauutiskirje %m/%y")
     html_start = html_start.replace("TITLE", title)
 
-    movie_entries = []
     section_dict = {"Watchlist": [], "Tykätyt": []}
 
     for movie in movies:
@@ -30,49 +32,49 @@ def populate_html(movies):
         "Watchlist")}, {"name": "Tykätyt", "movies": section_dict.pop("Tykätyt")}]
     sections += [{"name": key, "movies": value}
                  for key, value in section_dict.items()]
+
+    section_entries = []
     for section in sections:
+        movie_section_copy = copy.copy(movie_section)
+        movie_entries = []
         for movie in section["movies"]:
-            movie_template.find("h2").string.replace_with(
+            movie_copy = copy.copy(movie_template)
+            movie_copy.find("h2").string.replace_with(
                 movie["og_title"])
-
-            # Remove the br elements from the previous iteration
-            for t in movie_template.a.findChildren():
-                t.extract()
-            movie_template.a.smooth()
-
-            for t in movie_template.find("p", attrs={"css-class": "dates"}).findChildren():
-                t.extract()
-            movie_template.find("p", attrs={"css-class": "dates"}).smooth()
-
-            movie_template.find("em").string.replace_with(
+            movie_copy.find("em").string.replace_with(
                 movie["letterboxd_info"]["director"])
 
             # Weird, because of different formats
             dates = ", ".join(movie["date"]).split(", ")
-            movie_template.find("p", attrs={"css-class": "dates"}).string.replace_with(
+            movie_copy.find("p", attrs={"css-class": "dates"}).string.replace_with(
                 dates[0].title())
             for date in dates[1:]:
-                movie_template.find(
+                movie_copy.find(
                     "p", attrs={"css-class": "dates"}).append(soup.new_tag("br"))
-                movie_template.find("p", attrs={"css-class": "dates"}).append(
+                movie_copy.find("p", attrs={"css-class": "dates"}).append(
                     soup.new_string(date.title()))
 
             if type(movie["theater"]) == list:
                 theater_name = movie["theater"][0]
-                movie_template.a.string.replace_with(theater_name)
+                movie_copy.a.string.replace_with(theater_name)
                 for theater in movie["theater"][1:]:
-                    movie_template.a.append(soup.new_tag("br"))
-                    movie_template.a.append(soup.new_string(theater))
+                    movie_copy.a.append(soup.new_tag("br"))
+                    movie_copy.a.append(soup.new_string(theater))
             else:
                 theater_name = movie["theater"]
-                movie_template.a.string.replace_with(theater_name)
+                movie_copy.a.string.replace_with(theater_name)
 
-            movie_template.a["href"] = movie["info"]["human_url"]
-            movie_template.find("a", string=re.compile("Letterboxd"))[
+            movie_copy.a["href"] = movie["info"]["human_url"]
+            movie_copy.find("a", string=re.compile("Letterboxd"))[
                 "href"] = movie["letterboxd_info"]["url"]
 
-            movie_template.img["src"] = movie["letterboxd_info"]["poster"]
-            movie_template.img["alt"] = movie["title"]
-            movie_entries.append(str(movie_template))
+            movie_copy.img["src"] = movie["letterboxd_info"]["poster"]
+            movie_copy.img["alt"] = movie["title"]
+            movie_entries.append(movie_copy)
+        movie_section_copy.find('div', attrs={'class': 'movie'}).replace_with(
+            *movie_entries)
+        section_entries.append(str(subtitle_section).replace("SUBTITLE", section["name"]) +
+                               "\n" + str(movie_section_copy))
 
-    return title, html_start + "\n".join(movie_entries) + html_end
+    html = html_start + "\n".join(section_entries) + html_end
+    return title, html
